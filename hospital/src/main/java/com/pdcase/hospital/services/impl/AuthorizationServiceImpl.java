748f0a1e187dfe2e303d5c8c581b03455184a7cf
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-12 20:02:04 -0300

    cria o service para autenticação

---
+++ hospital/src/main/java/com/pdcase/hospital/services/impl/AuthorizationServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pdcase.hospital.services.impl;
+
+import com.pdcase.hospital.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationServiceImpl implements UserDetailsService {
+    //UserDetailsService faz a consulta para achar o usuario
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
