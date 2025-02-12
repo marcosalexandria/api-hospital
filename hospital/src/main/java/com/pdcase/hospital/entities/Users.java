b8a2273f9dfbe13e6069167885124f9c45b4d6f2
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-12 19:48:20 -0300

    criando a entidade de usuario que fará o login

---
+++ hospital/src/main/java/com/pdcase/hospital/entities/Users.java
@@ -0,0 +1,70 @@
+package com.pdcase.hospital.entities;
+
+import com.pdcase.hospital.entities.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users implements UserDetails {
+    //UserDetails idetifica quail eh a classe que vai fazer a altenticacao
+
+    private String id;
+    private String login;
+    private String password;
+    private UserRole role;
+
+    //  para o spring ver as roles que o usuario tem e liber as classes que for permitido
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //Se a role do usuario for admin ele tera a role admin e user, se nao, ele tera soh user
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    /*
+    * Vieram todos os metodos como esta comentado, mas pelo curso fazer uma altenticacao basica, nao vai fazer verificacao soh no getUsername
+    * */
+    @Override
+    public boolean isAccountNonExpired() {
+        //return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        //return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //return UserDetails.super.isEnabled();
+        return true;
+    }
+}
