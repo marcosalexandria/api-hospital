972e50f0e2b231bc20bf6c8de59c820605e11ccc
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-12 20:27:02 -0300

    configurando a autenticação

--- hospital/src/main/java/com/pdcase/hospital/config/SecurityConfigurations.java
+++ hospital/src/main/java/com/pdcase/hospital/config/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.pdcase.hospital.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,9 +14,16 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // Todos os metodos POST em /fichas só serao acessados se a role for ADIM
+        /// Todas as outras qualquer usuario autenticado vai poder acessar
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/fichas").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
